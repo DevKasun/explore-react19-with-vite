debdc437e1e5eb3efce8527f03b50d5bcee51951
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-10 12:08:24 +0530

    Form action example with useActionState

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-// src/App.jsx
-import { useState } from "react";
-import ExpensiveComponent from "./components/PerformanceTest";
-import FormExample from "./components/FormExample";
 import FormActionExample from "./components/FormActionExample";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data] = useState([1, 2, 3, 4, 5]);
-
   return (
-    <div className="w-full grid grid-cols-2 bg-yellow-50">
-      <div className="p-8 flex flex-col gap-4 items-center">
-        <h3 className="text-2xl">Performance Test</h3>
-        <button
-          className="bg-blue-600 text-white p-2 rounded-md"
-          onClick={() => setCount(count + 1)}
-        >
-          Count: {count}
-        </button>
-        <ExpensiveComponent data={data} />
-      </div>
-      <div className="p-8 flex flex-col gap-4 items-center">
-        <h3 className="text-2xl">Form and input ref</h3>
-        <FormExample />
-      </div>
-      <div className="p-8 flex flex-col gap-4 items-center">
-        <h3 className="text-2xl">Form Action</h3>
-        <FormActionExample />
-      </div>
+    <div className="w-full min-h-screen flex flex-col items-center gap-8 place-items-center bg-yellow-50 p-8">
+      <h1 className="text-3xl">Form Actions in action 😉</h1>
+      <FormActionExample />
     </div>
   );
 }
--- src/components/FormActionExample.jsx
+++ src/components/FormActionExample.jsx
@@ -1,17 +1,46 @@
-import CustomInputFiled from "./CustomInputField";
+import { useActionState } from "react";
 
 function FormActionExample() {
-  const formAction = async (formData) => {
-    "use server";
-    console.log(formData.get("username"));
-  };
+  const [state, formActionFunc] = useActionState(
+    async (prevState, formData) => {
+      const name = formData.get("email");
+      const password = formData.get("password");
+
+      const attempts = (prevState?.attempts || 0) + 1;
+
+      console.log(name);
+      console.log(password);
+
+      return { message: "Login Success", attempts: attempts };
+    },
+  );
+
   return (
-    <form action={formAction} className="flex flex-col gap-4">
-      <CustomInputFiled name="username" placeholder="Enter your name" />
-      <button type="submit" className="bg-blue-600 text-white p-2 rounded-md">
-        Submit
-      </button>
-    </form>
+    <div className="flex flex-col gap-4">
+      <form action={formActionFunc} className="flex flex-col gap-4 min-w-72">
+        <input
+          type="email"
+          name="email"
+          placeholder="User Email"
+          className="border-2 border-solid border-black rounded-md p-2"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="User Password"
+          className="border-2 border-solid border-black rounded-md p-2"
+        />
+        <button
+          type="submit"
+          className="border-2 border-solid border-black rounded-md p-2 bg-black text-white"
+        >
+          Login
+        </button>
+      </form>
+
+      {state?.message && <p>{state.message}</p>}
+      {state?.attempts && <p>Login attempts: {state.attempts}</p>}
+    </div>
   );
 }
 
